60c4139c0c4a81227b2e8bf640257d8807d3e8a2
Mohmed_Yousry <53487925+mohmed-yousry@users.noreply.github.com>
2022-09-10 22:37:18 +0200

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,21 @@
+let allNavLinks = Array.from(document.querySelectorAll(".form-par .right ul li")) ; 
+let allContent = Array.from(document.querySelector(".form-par .content .right .content").children) ; 
+console.log(allContent)
+allNavLinks.forEach(e=> {
+    e.addEventListener("click"  , (tr) => {
+        allNavLinks.forEach(x=> {
+            x.classList.remove("active")
+        })
+        allContent.forEach(con => {
+            con.style.display = "none" ; 
+            if(con.getAttribute("data-lab") == e.getAttribute("data-targ")) {
+                con.style.cssText = "                    display: inherit !important;                "
+
+            }
+            e.classList.add("active")
+        })
+        // console.log(e.getAttribute("data-targ")) = 
+        // if()
+    })
+
+})
