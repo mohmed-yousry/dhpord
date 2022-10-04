d6e451b2cb6fa1a7f92307a734e3a046e309377d
Mohmed_Yousry <53487925+mohmed-yousry@users.noreply.github.com>
2022-10-04 12:33:26 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
 // swiper
+let nav  = document.querySelector("header nav") ; 
+let swiperHeader = document.querySelector(".swiper.mySwiper") ; 
+let headerMenue = document.querySelector(".header .menue") ; 
+let logo = document.querySelector("nav .logo img")
+let showMenue = document.querySelector("nav .show-menue")
+console.log(logo.clientHeight)
+console.log(showMenue)
+window.onload = () => {
+  showMenue.style.height = `${logo.clientHeight}px`
+  swiperHeader.style.top = `-${nav.clientHeight}px`
+  headerMenue.style.top = `${logo.clientHeight}px`
+  headerMenue.style.height = `calc(100vh - ${logo.clientHeight}px)`
+}
+
+
+window.onresize = () => {
+  swiperHeader.style.top = `-${nav.clientHeight}px`
+  headerMenue.style.top = `${logo.clientHeight}px`
+  headerMenue.style.height = `calc(100vh - ${logo.clientHeight}px)`
+  showMenue.style.height = `${logo.clientHeight}px`
+  // console.log(nav.clientHeight)
+}
+// console.log(nav.clientHeight)
+// console.log(swiperHeader)
 var swiper = new Swiper(".mySwiper", {
     direction: "vertical",
-    loop : true , 
+    loop : true ,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
