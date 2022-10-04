928d6decfb4f86bc165fe821647639eb4a1b59e0
Mohmed_Yousry <53487925+mohmed-yousry@users.noreply.github.com>
2022-10-04 17:24:28 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,11 +3,13 @@ let nav  = document.querySelector("header nav") ;
 let swiperHeader = document.querySelector(".swiper.mySwiper") ; 
 let headerMenue = document.querySelector(".header .menue") ; 
 let logo = document.querySelector("nav .logo img")
-let showMenue = document.querySelector("nav .show-menue")
-console.log(logo.clientHeight)
-console.log(showMenue)
+let showMenue = Array.from(document.querySelectorAll("nav .show-menue"))
+let allCanves = Array.from(document.querySelectorAll(".offcanvas")) ; 
+
 window.onload = () => {
-  showMenue.style.height = `${logo.clientHeight}px`
+  showMenue[0].style.height = `${logo.clientHeight}px` ;
+  showMenue[1].style.height = `${logo.clientHeight}px` ;
+
   swiperHeader.style.top = `-${nav.clientHeight}px`
   headerMenue.style.top = `${logo.clientHeight}px`
   headerMenue.style.height = `calc(100vh - ${logo.clientHeight}px)`
@@ -18,11 +20,11 @@ window.onresize = () => {
   swiperHeader.style.top = `-${nav.clientHeight}px`
   headerMenue.style.top = `${logo.clientHeight}px`
   headerMenue.style.height = `calc(100vh - ${logo.clientHeight}px)`
-  showMenue.style.height = `${logo.clientHeight}px`
-  // console.log(nav.clientHeight)
+  showMenue[0].style.height = `${logo.clientHeight}px` ;
+  showMenue[1].style.height = `${logo.clientHeight}px` ;
+
 }
-// console.log(nav.clientHeight)
-// console.log(swiperHeader)
+
 var swiper = new Swiper(".mySwiper", {
     direction: "vertical",
     loop : true ,
@@ -36,3 +38,47 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 // swiper
+
+// handle openCanves
+showMenue.forEach(e=> {
+  e.addEventListener("click" , () => {
+    if(e.classList.contains("no-show")) {
+      // console.log(true)
+      e.classList.toggle("no-show")
+      e.children[1].innerHTML = "إغلاق"
+     e.children[0].innerHTML = `
+     <svg xmlns="http://www.w3.org/2000/svg" width="22.628" height="22.627" viewBox="0 0 22.628 22.627">
+      <g id="Group_2497" data-name="Group 2497" transform="translate(-867.62 -983.586) rotate(45)">
+        <rect id="Rectangle_80" data-name="Rectangle 80" width="6" height="6" transform="translate(1322 69)" fill="#fff"/>
+        <rect id="Rectangle_81" data-name="Rectangle 81" width="6" height="6" transform="translate(1322 79)" fill="#BF202F"/>
+        <rect id="Rectangle_82" data-name="Rectangle 82" width="6" height="6" transform="translate(1322 89)" fill="#fff"/>
+        <rect id="Rectangle_84" data-name="Rectangle 84" width="6" height="6" transform="translate(1318 85) rotate(180)" fill="#fff"/>
+        <rect id="Rectangle_87" data-name="Rectangle 87" width="6" height="6" transform="translate(1332 79)" fill="#fff"/>
+      </g>
+    </svg>
+     `
+    } else {
+      // e.classList.add("no-show")
+      e.classList.toggle("no-show")
+      e.children[1].innerHTML = "القائمة"
+      e.children[0].innerHTML = `
+      <svg id="Menu" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+      <rect id="Rectangle_687" data-name="Rectangle 687" width="30" height="30" fill="none"></rect>
+      <g id="Group_2724" data-name="Group 2724" transform="translate(-1310 -67)">
+        <rect id="Rectangle_80" data-name="Rectangle 80" width="6" height="6" transform="translate(1322 69)" fill="#696969"></rect>
+        <rect id="Rectangle_81" data-name="Rectangle 81" width="6" height="6" transform="translate(1322 79)" fill="#696969"></rect>
+        <rect id="Rectangle_82" data-name="Rectangle 82" width="6" height="6" transform="translate(1322 89)" fill="#696969"></rect>
+        <rect id="Rectangle_83" data-name="Rectangle 83" width="6" height="6" transform="translate(1318 75) rotate(180)" fill="#696969"></rect>
+        <rect id="Rectangle_84" data-name="Rectangle 84" width="6" height="6" transform="translate(1318 85) rotate(180)" fill="#696969"></rect>
+        <rect id="Rectangle_85" data-name="Rectangle 85" width="6" height="6" transform="translate(1318 95) rotate(180)" fill="#696969"></rect>
+        <rect id="Rectangle_86" data-name="Rectangle 86" width="6" height="6" transform="translate(1332 69)" fill="#696969"></rect>
+        <rect id="Rectangle_87" data-name="Rectangle 87" width="6" height="6" transform="translate(1332 79)" fill="#696969"></rect>
+        <rect id="Rectangle_88" data-name="Rectangle 88" width="6" height="6" transform="translate(1332 89)" fill="#0D9642"></rect>
+      </g>
+    </svg>      `
+    }
+
+  })
+})
+
+
