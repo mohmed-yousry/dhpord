2896d931856c7af95ea33a9a04c78938c782e39f
Mohmed_Yousry <53487925+mohmed-yousry@users.noreply.github.com>
2022-10-04 17:24:44 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,84 +1,194 @@
-// swiper
-let nav  = document.querySelector("header nav") ; 
-let swiperHeader = document.querySelector(".swiper.mySwiper") ; 
-let headerMenue = document.querySelector(".header .menue") ; 
-let logo = document.querySelector("nav .logo img")
-let showMenue = Array.from(document.querySelectorAll("nav .show-menue"))
-let allCanves = Array.from(document.querySelectorAll(".offcanvas")) ; 
+// dagre 1 //selecting all required elements
+const dropArea = document.querySelector(".drag-area1"),
+dragText = dropArea.querySelector(".heder1"),
+// button = dropArea.querySelector(".openfile"),
+input = dropArea.querySelector(".input-imges-file1");
+let file; //this is a global variable and we'll use it inside multiple functions
 
-window.onload = () => {
-  showMenue[0].style.height = `${logo.clientHeight}px` ;
-  showMenue[1].style.height = `${logo.clientHeight}px` ;
+dropArea.onclick = ()=>{
+   
+    input.click(); //if user click on the button then the input also clicked
 
-  swiperHeader.style.top = `-${nav.clientHeight}px`
-  headerMenue.style.top = `${logo.clientHeight}px`
-  headerMenue.style.height = `calc(100vh - ${logo.clientHeight}px)`
+   
 }
 
+input.addEventListener("change", function(){
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = this.files[0];
+  dropArea.classList.add("active");
+  showFile(); //calling function
+});
 
-window.onresize = () => {
-  swiperHeader.style.top = `-${nav.clientHeight}px`
-  headerMenue.style.top = `${logo.clientHeight}px`
-  headerMenue.style.height = `calc(100vh - ${logo.clientHeight}px)`
-  showMenue[0].style.height = `${logo.clientHeight}px` ;
-  showMenue[1].style.height = `${logo.clientHeight}px` ;
 
-}
+//If user Drag File Over DropArea
+dropArea.addEventListener("dragover", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  dropArea.classList.add("active");
+  dragText.textContent = "Release to Upload File";
+});
 
-var swiper = new Swiper(".mySwiper", {
-    direction: "vertical",
-    loop : true ,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-// swiper
+//If user leave dragged File from DropArea
+dropArea.addEventListener("dragleave", ()=>{
+  dropArea.classList.remove("active");
+  dragText.textContent = "Drag & Drop to Upload File";
+});
 
-// handle openCanves
-showMenue.forEach(e=> {
-  e.addEventListener("click" , () => {
-    if(e.classList.contains("no-show")) {
-      // console.log(true)
-      e.classList.toggle("no-show")
-      e.children[1].innerHTML = "إغلاق"
-     e.children[0].innerHTML = `
-     <svg xmlns="http://www.w3.org/2000/svg" width="22.628" height="22.627" viewBox="0 0 22.628 22.627">
-      <g id="Group_2497" data-name="Group 2497" transform="translate(-867.62 -983.586) rotate(45)">
-        <rect id="Rectangle_80" data-name="Rectangle 80" width="6" height="6" transform="translate(1322 69)" fill="#fff"/>
-        <rect id="Rectangle_81" data-name="Rectangle 81" width="6" height="6" transform="translate(1322 79)" fill="#BF202F"/>
-        <rect id="Rectangle_82" data-name="Rectangle 82" width="6" height="6" transform="translate(1322 89)" fill="#fff"/>
-        <rect id="Rectangle_84" data-name="Rectangle 84" width="6" height="6" transform="translate(1318 85) rotate(180)" fill="#fff"/>
-        <rect id="Rectangle_87" data-name="Rectangle 87" width="6" height="6" transform="translate(1332 79)" fill="#fff"/>
-      </g>
-    </svg>
-     `
-    } else {
-      // e.classList.add("no-show")
-      e.classList.toggle("no-show")
-      e.children[1].innerHTML = "القائمة"
-      e.children[0].innerHTML = `
-      <svg id="Menu" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
-      <rect id="Rectangle_687" data-name="Rectangle 687" width="30" height="30" fill="none"></rect>
-      <g id="Group_2724" data-name="Group 2724" transform="translate(-1310 -67)">
-        <rect id="Rectangle_80" data-name="Rectangle 80" width="6" height="6" transform="translate(1322 69)" fill="#696969"></rect>
-        <rect id="Rectangle_81" data-name="Rectangle 81" width="6" height="6" transform="translate(1322 79)" fill="#696969"></rect>
-        <rect id="Rectangle_82" data-name="Rectangle 82" width="6" height="6" transform="translate(1322 89)" fill="#696969"></rect>
-        <rect id="Rectangle_83" data-name="Rectangle 83" width="6" height="6" transform="translate(1318 75) rotate(180)" fill="#696969"></rect>
-        <rect id="Rectangle_84" data-name="Rectangle 84" width="6" height="6" transform="translate(1318 85) rotate(180)" fill="#696969"></rect>
-        <rect id="Rectangle_85" data-name="Rectangle 85" width="6" height="6" transform="translate(1318 95) rotate(180)" fill="#696969"></rect>
-        <rect id="Rectangle_86" data-name="Rectangle 86" width="6" height="6" transform="translate(1332 69)" fill="#696969"></rect>
-        <rect id="Rectangle_87" data-name="Rectangle 87" width="6" height="6" transform="translate(1332 79)" fill="#696969"></rect>
-        <rect id="Rectangle_88" data-name="Rectangle 88" width="6" height="6" transform="translate(1332 89)" fill="#0D9642"></rect>
-      </g>
-    </svg>      `
+//If user drop File on DropArea
+dropArea.addEventListener("drop", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = event.dataTransfer.files[0];
+  showFile(); //calling function
+});
+
+function showFile(){
+  let fileType = file.type; //getting selected file type
+  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
+  if(validExtensions.includes(fileType)){ //if user selected file is an image file
+    let fileReader = new FileReader(); //creating new FileReader object
+    fileReader.onload = ()=>{
+      let fileURL = fileReader.result; //passing user file source in fileURL variable
+        // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
+        let imgTag = `<img src="${fileURL}" alt="image">  
+        <div style = "background-color:#00000047 ; font-size : x-large" class="overlay align-items-center justify-content-center text-white position-absolute start-0 top-0 w-100 h-100 d-none">
+        change click
+        </div>
+        `; //creating an img tag and passing user selected file source inside src attribute
+        dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+        document.querySelector(".overlay").addEventListener("click" , () => {
+          dropArea.classList.remove("active")
+          // imgTag = ""
+          // console.log()
+          document.querySelector(".overlay").parentElement.innerHTML = `
+              <header class="heder2 heder">Drag &amp; Drop to Upload File</header>
+              <span class="text-uppercase">or click</span>
+              <input class="input-imges-file2 input-imges-file" type="file" hidden="">
+          `
+        })
     }
+    fileReader.readAsDataURL(file);
+  }else{
+    alert("This is not an Image File!");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+  }
+}
+// dagre 1 //selecting all required elements
 
-  })
+
+let allArea = Array.from(document.querySelectorAll(".row-3 .drag-area")) ; 
+allArea.forEach(e=> {
+    const dropArea = e ;
+    const dragText = e.children[1] ;
+    const input = e.children[3] ;
+    let file; //this is a global variable and we'll use it inside multiple functions
+    // console.log(e.children[3])
+
+dropArea.onclick = ()=>{
+    input.click(); //if user click on the button then the input also clicked
+    
+}
+
+input.addEventListener("change", function(){
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = this.files[0];
+  dropArea.classList.add("active");
+  showFile(); //calling function
+});
+
+
+//If user Drag File Over DropArea
+dropArea.addEventListener("dragover", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  dropArea.classList.add("active");
+  dragText.textContent = "Release to Upload File";
+});
+
+//If user leave dragged File from DropArea
+dropArea.addEventListener("dragleave", ()=>{
+  dropArea.classList.remove("active");
+  dragText.textContent = "Drag & Drop to Upload File";
+});
+
+//If user drop File on DropArea
+dropArea.addEventListener("drop", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = event.dataTransfer.files[0];
+  showFile(); //calling function
+});
+
+function showFile(){
+  let fileType = file.type; //getting selected file type
+  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
+  if(validExtensions.includes(fileType)){ //if user selected file is an image file
+    let fileReader = new FileReader(); //creating new FileReader object
+    fileReader.onload = ()=>{
+      let fileURL = fileReader.result; //passing user file source in fileURL variable
+        // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
+      let imgTag = `<img src="${fileURL}" alt="image">  
+      <div style = "background-color:#00000047 ; font-size : x-large" class="overlay align-items-center justify-content-center text-white position-absolute start-0 top-0 w-100 h-100 d-none">
+      change click
+      </div>
+      `; //creating an img tag and passing user selected file source inside src attribute
+      dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+      document.querySelector(".overlay").addEventListener("click" , () => {
+        dropArea.classList.remove("active")
+        // imgTag = ""
+        // console.log()
+        document.querySelector(".overlay").parentElement.innerHTML = `
+            <header class="heder2 heder">Drag &amp; Drop to Upload File</header>
+            <span class="text-uppercase">or click</span>
+            <input class="input-imges-file2 input-imges-file" type="file" hidden="">
+        `
+      })
+    }
+    fileReader.readAsDataURL(file);
+  }else{
+    alert("This is not an Image File!");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+  }
+}
 })
 
 
+// handle select gender
+let selectGender = document.querySelector(".select-gender") ; 
+let allOptionGender = Array.from(document.querySelectorAll(".select-gender option"))
+selectGender.addEventListener("change" ,(tr) => {
+
+  if(tr.target.value == "انثى") {
+    selectGender.parentElement.children[0].innerHTML = `
+    <path id="Path_14301" data-name="Path 14301" d="M-197.259,300.456a4.36,4.36,0,0,1-4.36,4.36,4.36,4.36,0,0,1-4.36-4.36,4.36,4.36,0,0,1,4.36-4.36A4.36,4.36,0,0,1-197.259,300.456Zm-4.36,4.36v4.274m2.137-2.136h-4.275" transform="translate(206.479 -295.596)" fill="none" stroke="#BF202F" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1"/>
+    `
+    console.log("fam")
+  } else {
+    selectGender.parentElement.children[0].innerHTML = `
+    <g id="Group_2553" data-name="Group 2553" transform="translate(197.206 -296.601)">
+    <path id="Path_14300" data-name="Path 14300" d="M-188.344,304.683a4.359,4.359,0,0,1-4.36,4.36,4.359,4.359,0,0,1-4.359-4.36,4.358,4.358,0,0,1,4.359-4.359A4.359,4.359,0,0,1-188.344,304.683Zm3.75-3.148v-4.274h-4.274m4.274,0-4.723,4.723" transform="translate(0.357 0.047)" fill="none" stroke="#bf202f" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1"></path>
+    </g>
+    `
+  }
+})
+
+
+// handle select gender
+
+// edit logo 
+
+let links = document.querySelector("nav .links") ; 
+let container = document.querySelector(".container") ; 
+window.onload = () => {
+  let mar = (window.innerWidth - container.clientWidth ) / 2 ; 
+  links.style.marginLeft = `${mar}px`
+}
+window.addEventListener("resize" , () => {
+  let mar = (window.innerWidth - container.clientWidth ) / 2 ; 
+  links.style.marginLeft = `${mar}px`
+  if(window.innerWidth < 767) {
+  }
+
+})
+
+// edit logo 
