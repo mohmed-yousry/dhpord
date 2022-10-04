cb56ae1f29d0588f82f0a0e8a4fe24904bba75e7
Mohmed_Yousry <53487925+mohmed-yousry@users.noreply.github.com>
2022-10-04 12:34:59 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -26,6 +26,10 @@ window.onresize = () => {
 var swiper = new Swiper(".mySwiper", {
     direction: "vertical",
     loop : true ,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
